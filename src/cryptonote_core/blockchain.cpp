25aa25691e23161c10ca49075ed3592b9319cc66
oxynaz <oxynaz@users.noreply.github.com>
2026-02-13 18:31:32 +0100

    V27: fix HF timestamps (must be incremental), original_version=17, HF heights reset to genesis

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -309,11 +309,11 @@ bool Blockchain::init(BlockchainDB* db, const network_type nettype, bool offline
   if (m_hardfork == nullptr)
   {
     if (m_nettype ==  FAKECHAIN || m_nettype == STAGENET)
-      m_hardfork = new HardFork(*db, 1, 0);
+      m_hardfork = new HardFork(*db, 17, 0);
     else if (m_nettype == TESTNET)
-      m_hardfork = new HardFork(*db, 1, testnet_hard_fork_version_1_till);
+      m_hardfork = new HardFork(*db, 17, testnet_hard_fork_version_1_till);
     else
-      m_hardfork = new HardFork(*db, 1, mainnet_hard_fork_version_1_till);
+      m_hardfork = new HardFork(*db, 17, mainnet_hard_fork_version_1_till);
   }
   if (m_nettype == FAKECHAIN)
   {
--- src/hardforks/hardforks.cpp
+++ src/hardforks/hardforks.cpp
@@ -4,28 +4,29 @@
 #undef MONERO_DEFAULT_LOG_CATEGORY
 #define MONERO_DEFAULT_LOG_CATEGORY "blockchain.hardforks"
 
-// C64 CHAIN hard forks
-// HF17: genesis (RandomWOW, dynamic unlock)
+// C64 CHAIN hard forks - all active from genesis
+// HF17: RandomWOW
 // HF18: fixed unlock time (288 blocks)
+// HF19: tokenomics (19.64M supply, vesting 4x25%)
 const hardfork_t mainnet_hard_forks[] = {
-  { 17, 1, 1, 1700000000 },
-  { 18, 1300, 1, 1700000000 },
-  { 19, 1500, 1, 1700000000 },
+  { 17, 0, 1, 1700000000 },
+  { 18, 2, 1, 1700000001 },
+  { 19, 3, 1, 1700000002 },
 };
 const size_t num_mainnet_hard_forks = sizeof(mainnet_hard_forks) / sizeof(mainnet_hard_forks[0]);
 const uint64_t mainnet_hard_fork_version_1_till = 0;
 
 const hardfork_t testnet_hard_forks[] = {
-  { 17, 1, 1, 1700000000 },
-  { 18, 1300, 1, 1700000000 },
-  { 19, 1500, 1, 1700000000 },
+  { 17, 0, 1, 1700000000 },
+  { 18, 2, 1, 1700000001 },
+  { 19, 3, 1, 1700000002 },
 };
 const size_t num_testnet_hard_forks = sizeof(testnet_hard_forks) / sizeof(testnet_hard_forks[0]);
 const uint64_t testnet_hard_fork_version_1_till = 0;
 
 const hardfork_t stagenet_hard_forks[] = {
-  { 17, 1, 1, 1700000000 },
-  { 18, 1300, 1, 1700000000 },
-  { 19, 1500, 1, 1700000000 },
+  { 17, 0, 1, 1700000000 },
+  { 18, 2, 1, 1700000001 },
+  { 19, 3, 1, 1700000002 },
 };
 const size_t num_stagenet_hard_forks = sizeof(stagenet_hard_forks) / sizeof(stagenet_hard_forks[0]);

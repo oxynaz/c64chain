9cf785386fca9c01870d1dbb60511597c3a26c5a
oxynaz <oxynaz@users.noreply.github.com>
2026-02-13 15:36:33 +0100

    V25b: fix HF voting threshold (0=default 80%, changed to 1%), window 100

--- src/hardforks/hardforks.cpp
+++ src/hardforks/hardforks.cpp
@@ -8,24 +8,24 @@
 // HF17: genesis (RandomWOW, dynamic unlock)
 // HF18: fixed unlock time (288 blocks)
 const hardfork_t mainnet_hard_forks[] = {
-  { 17, 1, 0, 1700000000 },
-  { 18, 1300, 0, 1700000000 },
-  { 19, 1500, 0, 1700000000 },
+  { 17, 1, 1, 1700000000 },
+  { 18, 1300, 1, 1700000000 },
+  { 19, 1500, 1, 1700000000 },
 };
 const size_t num_mainnet_hard_forks = sizeof(mainnet_hard_forks) / sizeof(mainnet_hard_forks[0]);
 const uint64_t mainnet_hard_fork_version_1_till = 0;
 
 const hardfork_t testnet_hard_forks[] = {
-  { 17, 1, 0, 1700000000 },
-  { 18, 1300, 0, 1700000000 },
-  { 19, 1500, 0, 1700000000 },
+  { 17, 1, 1, 1700000000 },
+  { 18, 1300, 1, 1700000000 },
+  { 19, 1500, 1, 1700000000 },
 };
 const size_t num_testnet_hard_forks = sizeof(testnet_hard_forks) / sizeof(testnet_hard_forks[0]);
 const uint64_t testnet_hard_fork_version_1_till = 0;
 
 const hardfork_t stagenet_hard_forks[] = {
-  { 17, 1, 0, 1700000000 },
-  { 18, 1300, 0, 1700000000 },
-  { 19, 1500, 0, 1700000000 },
+  { 17, 1, 1, 1700000000 },
+  { 18, 1300, 1, 1700000000 },
+  { 19, 1500, 1, 1700000000 },
 };
 const size_t num_stagenet_hard_forks = sizeof(stagenet_hard_forks) / sizeof(stagenet_hard_forks[0]);

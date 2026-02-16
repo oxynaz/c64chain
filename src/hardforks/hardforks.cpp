2519ba7361a5f89c7388830c5820088a7f9e7dbf
oxynaz <oxynaz@users.noreply.github.com>
2026-02-16 11:59:20 +0100

    v0.7.0: HF20 at block 2100 - LWMA-1 difficulty algorithm
    - Add HF20 activation at block 2100 in hardforks.cpp
    - LWMA-1 (next_difficulty_v6) activates for version >= 20
    - All blocks before 2100 validated with existing algorithm
    - Bump version to 0.7.0

--- src/hardforks/hardforks.cpp
+++ src/hardforks/hardforks.cpp
@@ -8,10 +8,12 @@
 // HF17: RandomWOW
 // HF18: fixed unlock time (288 blocks)
 // HF19: tokenomics (19.64M supply, vesting 4x25%)
+// HF20: LWMA-1 difficulty algorithm (block 2100)
 const hardfork_t mainnet_hard_forks[] = {
   { 17, 0, 1, 1700000000 },
   { 18, 2, 1, 1700000001 },
   { 19, 3, 1, 1700000002 },
+  { 20, 2100, 1, 1739739600 },
 };
 const size_t num_mainnet_hard_forks = sizeof(mainnet_hard_forks) / sizeof(mainnet_hard_forks[0]);
 const uint64_t mainnet_hard_fork_version_1_till = 0;
@@ -20,6 +22,7 @@ const hardfork_t testnet_hard_forks[] = {
   { 17, 0, 1, 1700000000 },
   { 18, 2, 1, 1700000001 },
   { 19, 3, 1, 1700000002 },
+  { 20, 2100, 1, 1739739600 },
 };
 const size_t num_testnet_hard_forks = sizeof(testnet_hard_forks) / sizeof(testnet_hard_forks[0]);
 const uint64_t testnet_hard_fork_version_1_till = 0;
@@ -28,5 +31,6 @@ const hardfork_t stagenet_hard_forks[] = {
   { 17, 0, 1, 1700000000 },
   { 18, 2, 1, 1700000001 },
   { 19, 3, 1, 1700000002 },
+  { 20, 2100, 1, 1739739600 },
 };
 const size_t num_stagenet_hard_forks = sizeof(stagenet_hard_forks) / sizeof(stagenet_hard_forks[0]);
